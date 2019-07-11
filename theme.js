16762acf8ce0e85add8fa94cf5b169a37d91c0a0
Dave Follett <davefollett@gmail.com>
2019-07-10 23:42:05 -0400

    first commit and rough draft

---
+++ theme.js
@@ -0,0 +1,20 @@
+import { condensed as theme } from "mdx-deck/themes";
+//import { future as theme } from "mdx-deck/themes";
+
+export default {
+  ...theme,
+  css: {
+    textAlign: 'left',
+  }
+}
+
+/*
+quote: {
+  background: '#f9f9f9',
+  borderLeft: '10px solid #ccc',
+  margin: '1.5em 10px',
+  padding: '0.5em 10px',
+  content: 'open-quote'
+  //quotes: ''"\201C""\201D""\2018""\2019"'
+}
+*/
